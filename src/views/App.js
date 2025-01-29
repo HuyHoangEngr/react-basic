61dbd4003e14eb335b8716d0cbbe48c83d1037f4
HuyHoangEngr <alegiahuy11@gmail.com>
2025-01-29 21:49:21 +0700

    video4.Class Component fix

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
-import MyComponent from './Examples/Mycomponent';
+import MyComponent from './Examples/MyComponent'
 
 function App() {
   return (
