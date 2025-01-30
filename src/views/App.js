a486c094637ea5985424c3192589e37777a7bc6d
HuyHoangEngr <alegiahuy11@gmail.com>
2025-01-30 23:24:25 +0700

    video9.Forms in React.JS

--- src/views/App.js
+++ src/views/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from './Examples/MyComponent'
+/**
+ * 2 components: class component / function component ( function, arrow)
+ * JSX
+ */
 
 function App() {
+  // const  App = () =>  {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world with React.js
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
         <MyComponent />
+
       </header>
     </div>
   );
