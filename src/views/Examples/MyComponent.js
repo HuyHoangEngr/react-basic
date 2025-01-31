683e343e8c905372891f53d2f643071a365ea120
HuyHoangEngr <alegiahuy11@gmail.com>
2025-01-31 12:25:05 +0700

    video11. Props

--- src/views/Examples/MyComponent.js
+++ src/views/Examples/MyComponent.js
@@ -54,9 +54,11 @@ class MyComponent extends React.Component {
                     />
                 </form>
 
-                <ChildComponent name={'child one'} />
-                <ChildComponent name={'child two'} />
-                <ChildComponent name={'child three'} />
+                <ChildComponent
+                    name={'Huy'}
+                    age={'30'}
+
+                />
             </>
         )
 
