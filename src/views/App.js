ebc0be54888aaee32718406fba78613276d62c3d
HuyHoangEngr <alegiahuy11@gmail.com>
2025-02-02 11:41:04 +0700

    video21.1.Xay dung ToDo APP PRO voi React.JS - CREATE/READ

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
-import MyComponent from './Examples/MyComponent'
+import MyComponent from './Examples/MyComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 /**
  * 2 components: class component / function component ( function, arrow)
  * JSX
@@ -13,13 +17,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world with React.js
-
+          Simple TODO Apps with React.js
         </p>
 
-        <MyComponent />
+        {/* <MyComponent /> */}
+        <ListTodo />
 
       </header>
+
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
