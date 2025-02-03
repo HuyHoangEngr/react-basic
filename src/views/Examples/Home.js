a56b3dc4ccd3591bfd3e890febb852f9927e8210
HuyHoangEngr <alegiahuy11@gmail.com>
2025-02-03 20:45:19 +0700

    video29.Importing Image - Su Dung Hinh Anh Trong Ung Dung React

--- src/views/Examples/Home.js
+++ src/views/Examples/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import avatar from '../../assets/images/avatar.jpg'
 
 class Home extends React.Component {
 
@@ -14,9 +15,14 @@ class Home extends React.Component {
     render() {
         console.log('>>> check props: ', this.props)
         return (
-            <div>
-                Hello world from Homepage
-            </div>
+            <>
+                <div>
+                    Hello world from Homepage
+                </div>
+                <div>
+                    <img src={avatar} style={{ width: '200px', height: '200px', marginTop: '20px' }} />
+                </div>
+            </>
         )
     }
 }
